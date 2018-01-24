100a519161634365bf2bbbf5964f88a1518741a6
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-01-24 19:41:47 +0000

    Corrected name, s/subnetwork/subnet/

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -3,5 +3,5 @@ module "terraform-gcp-natgateway" {
   project = "${var.project_id}"
   region = "${var.region}"
   network = "${var.env_id}-network"
-  subnetwork = "${var.env_id}-subnetwork"
+  subnetwork = "${var.env_id}-subnet"
 }
