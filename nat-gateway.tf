5605566d90938acca3f8d190876a608eb102c875
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-01-24 19:11:26 +0000

    Using env rather than project for network references

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -2,6 +2,6 @@ module "terraform-gcp-natgateway" {
   source = "github.com/migs/terraform-gcp-natgateway"
   project = "${var.project_id}"
   region = "${var.region}"
-  network = "${var.project_id}-network"
-  subnetwork = "${var.project_id}-subnetwork"
+  network = "${var.env_id}-network"
+  subnetwork = "${var.env_id}-subnetwork"
 }
