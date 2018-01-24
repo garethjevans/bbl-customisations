b241c3edf26868f89e9c0be9a56fd3bb3e64502b
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-01-24 15:41:08 +0000

    Using full git url

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,5 +1,5 @@
 module "terraform-gcp-natgateway" {
-  source = "../terraform-gcp-natgateway"
+  source = "https://github.com/migs/terraform-gcp-natgateway"
   project = "${var.project}"
   region = "${var.region}"
 }
