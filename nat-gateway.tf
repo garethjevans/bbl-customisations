4583b59b435e4d28408830c1233d41acea070579
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-01-24 19:07:07 +0000

    Changed network for nat-gateway

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -2,4 +2,6 @@ module "terraform-gcp-natgateway" {
   source = "github.com/migs/terraform-gcp-natgateway"
   project = "${var.project_id}"
   region = "${var.region}"
+  network = "${var.project_id}-network"
+  subnetwork = "${var.project_id}-subnetwork"
 }
