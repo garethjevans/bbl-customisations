1d348a17eda0e73b762f51bd5629773beff20bcc
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-01-31 19:59:36 +0000

    Renamed database script, added nat-instance ip as auth'd network

--- database.tf
+++ database.tf
@@ -42,7 +42,7 @@ resource "google_sql_database_instance" "cf-db-instance" {
       ipv4_enabled = "true"
 
       authorized_networks = [
-        { value = "0.0.0.0/0" },
+        { value = "${module.terraform-gcp-natgateway.nat-gateway-ips["0"]}" },
       ]
     }
   }
--- storage.tf
+++ storage.tf
@@ -1,26 +1,26 @@
 resource "google_storage_bucket" "buildpack" {
-    name = "${var.projectid}-buildpack"
+    name = "${var.env_id}-buildpack"
     location = "${var.region}"
     storage_class = "REGIONAL"
     force_destroy = true
 }
 
 resource "google_storage_bucket" "droplet" {
-    name = "${var.projectid}-droplet"
+    name = "${var.env_id}-droplet"
     location = "${var.region}"
     storage_class = "REGIONAL"
     force_destroy = true
 }
 
 resource "google_storage_bucket" "package" {
-    name = "${var.projectid}-package"
+    name = "${var.env_id}-package"
     location = "${var.region}"
     storage_class = "REGIONAL"
     force_destroy = true
 }
 
 resource "google_storage_bucket" "resource" {
-    name = "${var.projectid}-resource"
+    name = "${var.env_id}-resource"
     location = "${var.region}"
     storage_class = "REGIONAL"
     force_destroy = true
